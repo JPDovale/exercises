062ec1cbb645ea3f576eb116ada02b74ee93417b
João Paulo do Vale <100259992+JPDovale@users.noreply.github.com>
2023-08-20 20:14:54 -0300

    imports

---
+++ src/fundations/Imports.java
@@ -0,0 +1,15 @@
+package fundations;
+
+import java.util.Date;
+
+public class Imports {
+	public static void main(String[] args) {
+		String s = "Bom dia!";
+		System.out.println(s);
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+//		JButton button = new JButton();
+	}
+}
